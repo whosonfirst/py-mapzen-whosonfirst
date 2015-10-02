1052569fffec3c658f291d26b1d571c470ce9ff1
thisisaaronland <aaron@mapzen.com>
2015-10-02 20:48:08 +0000

    require mapzen.whosonfirst.concordances-0.02; bump to 0.08

--- setup.py
+++ setup.py
@@ -8,12 +8,12 @@ desc = open("README.md").read(),
 setup(
     name='mapzen.whosonfirst.bundle',
     namespace_packages=[],
-    version='0.07',
+    version='0.08',
     description='A package to install all the other Who\'s On First Python packages',
     author='Mapzen',
     url='https://github.com/mapzen/py-mapzen-whosonfirst-bundle',
     install_requires=[
-        'mapzen.whosonfirst.concordances>=0.01',
+        'mapzen.whosonfirst.concordances>=0.02',
         'mapzen.whosonfirst.export>=0.61',
         'mapzen.whosonfirst.geojson>=0.03',
         'mapzen.whosonfirst.languages>=0.02',
@@ -28,7 +28,7 @@ setup(
         "mapzen.whosonfirst.aws>=0.01",
         ],
     dependency_links=[
-        'https://github.com/whosonfirst/py-mapzen-whosonfirst-concordances/tarball/master#egg=mapzen.whosonfirst.concordances-0.01',
+        'https://github.com/whosonfirst/py-mapzen-whosonfirst-concordances/tarball/master#egg=mapzen.whosonfirst.concordances-0.02',
         'https://github.com/whosonfirst/py-mapzen-whosonfirst-export/tarball/master#egg=mapzen.whosonfirst.export-0.61',
         'https://github.com/whosonfirst/py-mapzen-whosonfirst-geojson/tarball/master#egg=mapzen.whosonfirst.geojson-0.03',
         'https://github.com/whosonfirst/py-mapzen-whosonfirst-languages/tarball/master#egg=mapzen.whosonfirst.languages-0.02',
@@ -45,5 +45,5 @@ setup(
     packages=packages,
     scripts=[
         ],
-    download_url='https://github.com/mapzen/py-mapzen-whosonfirst-bundle/releases/tag/v0.07',
+    download_url='https://github.com/mapzen/py-mapzen-whosonfirst-bundle/releases/tag/v0.08',
     license='BSD')
