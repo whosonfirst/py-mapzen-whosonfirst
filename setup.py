239c32a673349d7cce0822484e88adeec53fbe4c
thisisaaroland <aaron@mapzen.com>
2016-01-18 18:24:41 -0800

    update concordances/spatial to not enforce pscopg because OS X; bump to 0.25

--- setup.py
+++ setup.py
@@ -8,12 +8,12 @@ desc = open("README.md").read(),
 setup(
     name='mapzen.whosonfirst.bundle',
     namespace_packages=[],
-    version='0.24',
+    version='0.25',
     description='A package to install all the other Who\'s On First Python packages',
     author='Mapzen',
     url='https://github.com/mapzen/py-mapzen-whosonfirst-bundle',
     install_requires=[
-        'mapzen.whosonfirst.concordances>=0.05',
+        'mapzen.whosonfirst.concordances>=0.06',
         'mapzen.whosonfirst.export>=0.71',
         'mapzen.whosonfirst.geojson>=0.06',
         'mapzen.whosonfirst.languages>=0.02',
@@ -24,7 +24,7 @@ setup(
         'mapzen.whosonfirst.placetypes>=0.11',
         'mapzen.whosonfirst.search>=0.11',
         'mapzen.whosonfirst.sources>=0.03',
-        'mapzen.whosonfirst.spatial>=0.11',
+        'mapzen.whosonfirst.spatial>=0.12',
         'mapzen.whosonfirst.utils>=0.17',
         "mapzen.whosonfirst.validator>=0.10",
         "mapzen.whosonfirst.chatterbox>=0.01",
@@ -32,7 +32,7 @@ setup(
         "slack.api>=0.04",
         ],
     dependency_links=[
-        'https://github.com/whosonfirst/py-mapzen-whosonfirst-concordances/tarball/master#egg=mapzen.whosonfirst.concordances-0.05',
+        'https://github.com/whosonfirst/py-mapzen-whosonfirst-concordances/tarball/master#egg=mapzen.whosonfirst.concordances-0.06',
         'https://github.com/whosonfirst/py-mapzen-whosonfirst-export/tarball/master#egg=mapzen.whosonfirst.export-0.71',
         'https://github.com/whosonfirst/py-mapzen-whosonfirst-geojson/tarball/master#egg=mapzen.whosonfirst.geojson-0.06',
         'https://github.com/whosonfirst/py-mapzen-whosonfirst-languages/tarball/master#egg=mapzen.whosonfirst.languages-0.02',
@@ -43,7 +43,7 @@ setup(
         'https://github.com/whosonfirst/py-mapzen-whosonfirst-placetypes/tarball/master#egg=mapzen.whosonfirst.placetypes-0.11',
         'https://github.com/whosonfirst/py-mapzen-whosonfirst-search/tarball/master#egg=mapzen.whosonfirst.search-0.11',
         'https://github.com/whosonfirst/py-mapzen-whosonfirst-sources/tarball/master#egg=mapzen.whosonfirst.sources-0.03',
-        'https://github.com/whosonfirst/py-mapzen-whosonfirst-spatial/tarball/master#egg=mapzen.whosonfirst.spatial-0.11',
+        'https://github.com/whosonfirst/py-mapzen-whosonfirst-spatial/tarball/master#egg=mapzen.whosonfirst.spatial-0.12',
         'https://github.com/whosonfirst/py-mapzen-whosonfirst-utils/tarball/master#egg=mapzen.whosonfirst.utils-0.17',
         'https://github.com/whosonfirst/py-mapzen-whosonfirst-validator/tarball/master#egg=mapzen.whosonfirst.validator-0.10',
         'https://github.com/whosonfirst/py-mapzen-whosonfirst-chatterbox/tarball/master#egg=mapzen.whosonfirst.chatterbox-0.01',
@@ -53,5 +53,5 @@ setup(
     packages=packages,
     scripts=[
         ],
-    download_url='https://github.com/mapzen/py-mapzen-whosonfirst-bundle/releases/tag/v0.24',
+    download_url='https://github.com/mapzen/py-mapzen-whosonfirst-bundle/releases/tag/v0.25',
     license='BSD')
