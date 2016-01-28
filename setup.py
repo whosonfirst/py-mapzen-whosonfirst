c146a3eea3b9f681748853ad5446b0658aea7369
thisisaaronland <aaron@mapzen.com>
2016-01-28 22:27:39 +0000

    build requires/install links on the fly using MAPZEN.REQUIRES.json

--- setup.py
+++ setup.py
@@ -3,8 +3,9 @@
 # Remove .egg-info directory if it exists, to avoid dependency problems with
 # partially-installed packages (20160119/dphiffer)
 
-import os, sys
-from shutil import rmtree
+import os
+import sys
+import shutil
 
 setup = os.path.abspath(sys.argv[0])
 parent = os.path.dirname(setup)
@@ -18,15 +19,22 @@ if pkg.startswith("py-mapzen"):
     egg_info = os.path.join(parent, egg_info)
 
     if os.path.exists(egg_info):
-        rmtree(egg_info)
+        shutil.rmtree(egg_info)
 
 # Okay carry on as usual... well, except for all the requires stuff
 # below (20160128/thisisaaronland)
 
 # See also: scripts/build-mapzen-requires.py
 
+import json
+
+# please to list your not-MAPZEN.REQUIRES.txt dependencies here - the format is
+# "<PACKAGE> and optional versioning" + ":" + "optional dependency/install link"
+# py-mapzen specific stuff is handled separately afterwards by reading the
+# MAPZEN.REQUIRES.json file (20160128/thisisaaronland)
+
 deps = {
-    'slack.api>=0.04' => 'https://github.com/whosonfirst/py-slack-api/tarball/master#egg=slack.api-0.04'
+    'slack.api>=0.04':'https://github.com/whosonfirst/py-slack-api/tarball/master#egg=slack.api-0.04'
 }
 
 requires=[]
@@ -35,7 +43,7 @@ links=[]
 mz_deps = os.path.join(parent, "MAPZEN.REQUIRES.json")
 
 if not os.path.exists(mz_deps):
-    raise Exception, "%s does not exist - please run scripts/build-mapzen-requires.py" % mz_deps)
+    raise Exception, "%s does not exist - please run scripts/build-mapzen-requires.py" % mz_deps
 
 mz_fh = open(mz_deps, "r")
 mz_data = json.load(mz_fh)
@@ -47,7 +55,7 @@ for pkg, url in deps.items():
 
     requires.append(pkg)
 
-    if v != '':
+    if url != '':
         links.append(url)
 
 # Okay... now we carry on as is...
@@ -65,48 +73,8 @@ setup(
     description='A package to install all the other Who\'s On First Python packages',
     author='Mapzen',
     url='https://github.com/whosonfirst/py-mapzen-whosonfirst',
-    install_requires=[
-        'mapzen.whosonfirst.bundles>=0.05',
-        'mapzen.whosonfirst.concordances>=0.06',
-        'mapzen.whosonfirst.diff>=0.03',
-        'mapzen.whosonfirst.export>=0.71',
-        'mapzen.whosonfirst.geojson>=0.06',
-        'mapzen.whosonfirst.languages>=0.02',
-        'mapzen.whosonfirst.mapshaper>=0.04',
-        'mapzen.whosonfirst.meta>=0.09',
-        'mapzen.whosonfirst.names>=0.02',
-        'mapzen.whosonfirst.pip>=0.03',
-        'mapzen.whosonfirst.placetypes>=0.11',
-        'mapzen.whosonfirst.search>=0.12',
-        'mapzen.whosonfirst.sources>=0.03',
-        'mapzen.whosonfirst.spatial>=0.12',
-        'mapzen.whosonfirst.utils>=0.20',
-        "mapzen.whosonfirst.validator>=0.10",
-        "mapzen.whosonfirst.chatterbox>=0.01",
-        "mapzen.whosonfirst.aws>=0.02",
-        "slack.api>=0.04",
-        ],
-    dependency_links=[
-        'https://github.com/whosonfirst/py-mapzen-whosonfirst-bundles/tarball/master#egg=mapzen.whosonfirst.bundles-0.05',
-        'https://github.com/whosonfirst/py-mapzen-whosonfirst-concordances/tarball/master#egg=mapzen.whosonfirst.concordances-0.06',
-        'https://github.com/whosonfirst/py-mapzen-whosonfirst-diff/tarball/master#egg=mapzen.whosonfirst.diff-0.03',
-        'https://github.com/whosonfirst/py-mapzen-whosonfirst-export/tarball/master#egg=mapzen.whosonfirst.export-0.71',
-        'https://github.com/whosonfirst/py-mapzen-whosonfirst-geojson/tarball/master#egg=mapzen.whosonfirst.geojson-0.06',
-        'https://github.com/whosonfirst/py-mapzen-whosonfirst-languages/tarball/master#egg=mapzen.whosonfirst.languages-0.02',
-        'https://github.com/whosonfirst/py-mapzen-whosonfirst-mapshaper/tarball/master#egg=mapzen.whosonfirst.mapshaper-0.04',
-        'https://github.com/whosonfirst/py-mapzen-whosonfirst-meta/tarball/master#egg=mapzen.whosonfirst.meta-0.09',
-        'https://github.com/whosonfirst/py-mapzen-whosonfirst-names/tarball/master#egg=mapzen.whosonfirst.names-0.02',
-        'https://github.com/whosonfirst/py-mapzen-whosonfirst-pip/tarball/master#egg=mapzen.whosonfirst.pip-0.03',
-        'https://github.com/whosonfirst/py-mapzen-whosonfirst-placetypes/tarball/master#egg=mapzen.whosonfirst.placetypes-0.11',
-        'https://github.com/whosonfirst/py-mapzen-whosonfirst-search/tarball/master#egg=mapzen.whosonfirst.search-0.12',
-        'https://github.com/whosonfirst/py-mapzen-whosonfirst-sources/tarball/master#egg=mapzen.whosonfirst.sources-0.03',
-        'https://github.com/whosonfirst/py-mapzen-whosonfirst-spatial/tarball/master#egg=mapzen.whosonfirst.spatial-0.12',
-        'https://github.com/whosonfirst/py-mapzen-whosonfirst-utils/tarball/master#egg=mapzen.whosonfirst.utils-0.20',
-        'https://github.com/whosonfirst/py-mapzen-whosonfirst-validator/tarball/master#egg=mapzen.whosonfirst.validator-0.10',
-        'https://github.com/whosonfirst/py-mapzen-whosonfirst-chatterbox/tarball/master#egg=mapzen.whosonfirst.chatterbox-0.01',
-        'https://github.com/whosonfirst/py-mapzen-whosonfirst-aws/tarball/master#egg=mapzen.whosonfirst.aws-0.02',
-        'https://github.com/whosonfirst/py-slack-api/tarball/master#egg=slack.api-0.04',
-        ],
+    install_requires=requires,	# see above
+    dependency_links=links,	# no really, see above
     packages=packages,
     scripts=[
         ],
